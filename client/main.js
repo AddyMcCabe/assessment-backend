618fcb83cdb00503efc611b725b776fa39659771
Addison McCabe <addisonhartmccabe@gmail.com>
2022-05-06 15:33:40 -0700

    after review commit

--- client/main.js
+++ client/main.js
@@ -1,6 +1,9 @@
 const complimentBtn = document.getElementById("complimentButton")
-const fortuneBtn = document.getElementById('fortuneBtn')
-const submit = document.getElementById('submitBtn')
+const fortuneBtn = document.getElementById('fortune-button')
+const newFortuneBtn = document.getElementById('new-fortune-button')
+const newFortuneInput = document.getElementById('new-fortune-input')
+const deleteBtn = document.getElementById('delete-button')
+const deleteInput = document.getElementById('delete-input')
  
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -18,14 +21,27 @@ const getFortune = () => {
     })
 };
 
-const submitHandler = () => {
-    axios.post('http://localhost:4000/api/encouragement')
+const addFortune = () => {
+    axios.post('http://localhost:4000/api/fortune/', {newFortune: newFortuneInput.value})
     .then(res => {
         alert(res.data)
+        newFortuneInput.value=''
+    })
+};
+
+const deleteFortune = () => {
+    axios.delete(`http://localhost:4000/api/fortune/${deleteInput.value}`)
+    .then(res => {
+        alert(res.data);
+        deleteInput.value = ''
     })
 }
 
 
+
+
 fortuneBtn.addEventListener('click', getFortune)
 complimentBtn.addEventListener('click', getCompliment)
-submit.addEventListener('click', submitHandler)
+newFortuneBtn.addEventListener('click', addFortune)
+deleteBtn.addEventListener('click', deleteFortune )
+
--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
-let globalId = 1
+const fortunes = ['There is money in your future', 'Youre ability for accomplishment will follow with success', 'You will marry your lover', 'That bug bite from last week will bring super powers', 'Nothing can be seen about your future'];
+
 
 module.exports = {
 
@@ -13,13 +14,26 @@ module.exports = {
     },
 
     getFortune: (req, res) => {
-        const fortunes = ['There is money in your future', 'Youre ability for accomplishment will follow with success', 'You will marry your lover', 'That bug bite from last week will bring super powers', 'Nothing can be seen about your future'];
 
         let randomIndex = Math.floor(Math.random() * fortunes.length);
         let randomFortune = fortunes[randomIndex];
 
         res.status(200).send(randomFortune);
     },
+
+    addFortune: (req, res) => {
+        const {newFortune} = req.body
+
+        fortunes.push(newFortune);
+        res.status(200).send('fortune added');
+    },
+
+    deleteFortune: (req, res) => {
+        const {id} = req.params
+
+        fortunes.splice(id, 1);
+        res.status(200).send('fortune removed');
+    }
 
     
 
